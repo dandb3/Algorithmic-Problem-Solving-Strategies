e6f627606fbba53f81970f2fd17791b2bf6a4d5b
dandb3 <djw0409@naver.com>
2022-07-15 00:16:49 +0900

    커밋

---
+++ 종만북/종만북/LIS OPTIMIZED.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	FASTIO;
+
+	string str = "123";
+	cout << (str.substr(4) == "");
+
+
+	return 0;
+}
